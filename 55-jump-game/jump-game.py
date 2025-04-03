7153a99175e60ed18bf0ac022f5f23f0df2d4049
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-03 09:35:40 -0400

    Time: 41 ms (27.45%) | Memory: 13.2 MB (79.63%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -4,11 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        gas=0
+        """gas=0
         for value in nums:
             if (gas<0):
                 return False
             elif value>gas:
                 gas= value
             gas -=1
-        return True
+        return True"""
+        """Keep track of the farthest index you can reach.
+        Iterate through the array, updating the farthest reachable index at each step.
+        If at any index, the farthest reachable index is less than the current index, return False  (since you are stuck).
+        If the farthest reachable index is greater than or equal to the last index, return True."""
+        max_reachable,i=0,0
+        for i in range(len(nums)): 
+            if len(nums) == 1:
+                return True  # Single-element array means we're already at the last index
+
+            if (max_reachable < i):
+                return False
+            max_reachable=max(max_reachable,(i+nums[i]))
+            if max_reachable >= len(nums) - 1:  # Early exit if we can reach the end
+                return True
+        return False
