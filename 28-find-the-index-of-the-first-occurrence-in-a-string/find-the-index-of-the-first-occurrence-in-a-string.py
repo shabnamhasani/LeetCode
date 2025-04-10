8cee141d0af6601b05d5a81265ea21d31d1dd6b2
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-09 22:40:12 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.69%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,7 +5,11 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        for i in range(len(haystack)):
-            if haystack[i:].startswith(needle):
+        if len(haystack) < len(needle):
+            return -1
+        for i in range(len(haystack) - len(needle) + 1): #checking substrings of haystack starting at position i with length len(needle). So if you go too far, you'd go out of bounds.
+            if haystack[i:i+len(needle)].startswith(needle):
                 return i
         return -1
+
+        
