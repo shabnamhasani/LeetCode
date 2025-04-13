149fa22d8e56eca020ffd8254c896cde0165f862
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-12 22:59:32 -0400

    Time: 111 ms (21.14%) | Memory: 28.6 MB (24.19%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        """You’re looking for a taller line to potentially increase the area, even though the width shrinks."""
+        n=len(height)
+        p1,p2,max_area=0,n-1,0
+        
+        while (p1<p2):
+            max_area= max(max_area, min(height[p1],height[p2])*(p2-p1))
+            if height[p1]<height[p2]:
+                p1 +=1
+            else: 
+                p2 -=1
+        return max_area
