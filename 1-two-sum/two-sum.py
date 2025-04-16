56da7e0e41a048d4d8724e49e7e1c9d421453c2c
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-15 21:08:00 -0400

    Time: 0 ms (100.00%) | Memory: 19 MB (24.82%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        """For each element, calculate its complement (target - current_element). check if the complement is already in the hash map"""
+        hash_map={}
+        for indx, key in enumerate(nums):
+            Complement=target-key
+            if Complement in hash_map:
+                return [indx, hash_map[Complement]]
+            hash_map[key]=indx
+
+
+
+    
