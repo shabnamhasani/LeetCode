a5acdca00ed2e46c4111f8bea4bc3a940ee2a041
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-20 14:12:26 -0400

    Time: 50 ms (5.02%) | Memory: 28.7 MB (11.22%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,14 +1,37 @@
+import bisect
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """sliding window implemented by 2 pointers"""
+        # min_len=float('inf')
+        # left,total=0,0
+
+        # for right in range(len(nums)):
+        #     total +=nums[right]
+
+        #     while (total >=target):
+        #         min_len=min(min_len,right-left+1)
+        #         total -=nums[left]
+        #         left +=1
+
+        # return 0 if min_len==float('inf') else min_len
+        """binary search + prefix sum"""
+        n=len(nums)
         min_len=float('inf')
-        left,total=0,0
+        prefix=[0]*(n+1)
+        # Step 1: Build prefix sum array
+        for i in range (n):
+            prefix[i+1]=prefix[i]+nums[i]
 
-        for right in range(len(nums)):
-            total +=nums[right]
+        # Step 2: For each i, binary search the smallest j where prefix[j] >= prefix[i] + target
+        for i in range(n):
+            required=prefix[i] + target
+            # Binary search for the smallest index j where prefix[j] >= required
+            #With slicing (prefix[i:]), Python creates a copy of part of the list 
+            bound= bisect.bisect_left(prefix, required)
+            if bound <=n:
+                min_len=min(min_len, bound-i)
+        return 0 if min_len ==float('inf') else min_len
 
-            while (total >=target):
-                min_len=min(min_len,right-left+1)
-                total -=nums[left]
-                left +=1
 
-        return 0 if min_len==float('inf') else min_len
+
+
