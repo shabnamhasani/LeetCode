9e2aebd6a97dc250a8704897ea8a1e1f2736394e
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-20 17:00:00 -0400

    Time: 15 ms (81.86%) | Memory: 17.8 MB (82.74%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,11 +1,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen=set()
+        """We use **while** to remove all duplicates because we don’t know how many times a character might be duplicated in the window."""
+        # seen=set()
+        # left,max_len=0,0
+        # for right in range(len(s)):
+        #     while s[right] in seen:
+        #         seen.remove(s[left])
+        #         left +=1
+        #     seen.add(s[right])
+        #     max_len =max(max_len, right-left +1)
+        # return max_len
+
+        """Using a dictionary allows us to remember the last index where a character was seen and jump the left pointer directly past that"""
+        char_ind={}
         left,max_len=0,0
         for right in range(len(s)):
-            while s[right] in seen:
-                seen.remove(s[left])
-                left +=1
-            seen.add(s[right])
+            if s[right] in char_ind and char_ind[s[right]]>=left:
+                # Move left pointer to one after the previous occurrence
+                left=char_ind[s[right]]+1
+            # Update the character's index
+            char_ind[s[right]]=right
             max_len =max(max_len, right-left +1)
         return max_len
