8a364ea45e7663bc9107d94a2c41f4b288adeff2
shabnamhasani <shabnamhasani73@gmail.com>
2025-04-24 13:50:32 -0400

    Time: 5 ms (51.14%) | Memory: 18 MB (5.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,33 +1,29 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         return (self.is_row_valid (board) and self.is_col_valid(board) and self.is_box_valid(board))
-    def is_row_valid(self, board) -> bool:
-        rows = [set() for _ in range(9)]
-        for i in range(9):
-            for j in range(9):
-                val = board[i][j]
-                if val == '.':
-                    continue
+    def is_row_valid(self, board: List[List[str]]) -> bool:
 
-                # Check row
-                if val in rows[i]:
+        for row in board:
+            seen=set()
+            for val in row:
+                if val != '.' and val in seen:
                     return False
-                rows[i].add(val)
+                seen.add(val)
         return True
-    def is_col_valid(self, board) -> bool:
-        cols = [set() for _ in range(9)]
-        for i in range(9):
-            for j in range(9):
-                val = board[i][j]
-                if val == '.':
-                    continue
 
-                # Check column
-                if val in cols[j]:
+    def is_col_valid(self, board: List[List[str]]) -> bool:
+
+        for i in range(9):
+            seen=set()
+            for j in range(9):
+                val =board[j][i]
+                if val != '.' and val in seen:
                     return False
-                cols[j].add(val)
+                seen.add(val)
+
         return True
-    def is_box_valid(self, board) -> bool:
+
+    def is_box_valid(self, board: List[List[str]]) -> bool:
         boxes =[set() for _ in range(9)]
         for i in range(9):
             for j in range(9):
